8f85716bc632b4c473887ed51802a8ac5c3502ad
Gogs <ppp09812345@163.com>
2018-09-06 14:31:16 +0800

    update

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from django.urls import include
 
 urlpatterns = [
-    path('lib/',include('lib.urls')),
+    path('lib/', include('lib.urls')),
     path('admin/', admin.site.urls),
 ]
